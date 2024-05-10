f79b25033d93d6c66b860498a5d53b5ddfd0988f
tazim5032 <51076835+tazim5032@users.noreply.github.com>
2024-05-10 15:42:17 +0600

    find all assignmnent done

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 const corsOptions = {
-    origin: [
-        'http://localhost:5173',
-        'http://localhost:5174',
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
 
-    ],
-    credentials: true,
-    optionSuccessStatus: 200,
+  ],
+  credentials: true,
+  optionSuccessStatus: 200,
 }
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -34,17 +34,33 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    
+
+    const assignmentCollection = client.db('assignmentDB').collection('assignment');
+
+    //send data from createAssignment to db
+    app.post('/assignment', async (req, res) => {
+      console.log(req.body);
+      const result = await assignmentCollection.insertOne(req.body);
+      console.log(result);
+      res.send(result);
+    })
+
+    //find all assignment fromm db
+    app.get('/assignment', async (req, res) => {
+      const result = await assignmentCollection.find({}).toArray();
+      res.send(result);
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    
+
   }
 }
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Hello from task-titans Server....')
+  res.send('Hello from edu-flow Server....')
 })
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
