244b1e4eabb9e46d2d918e7be8204cd597987c8b
tazim5032 <51076835+tazim5032@users.noreply.github.com>
2024-05-13 19:42:54 +0600

    cookie parser added

--- index.js
+++ index.js
@@ -19,8 +19,7 @@ const corsOptions = {
 }
 app.use(cors(corsOptions));
 app.use(express.json());
-
-
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o4eqbyc.mongodb.net/?retryWrites=true&w=majority`
 
