f3fc012c36357f9171554cbd44b7f143669d1882
tazim5032 <51076835+tazim5032@users.noreply.github.com>
2024-05-13 11:58:06 +0600

    cookie parser added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
